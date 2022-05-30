958b7963151fe4538bc75ae8eccace8438357e65
andywurm <andaiyewurm@gmail.com>
2022-05-29 23:22:20 -0400

    finish skills

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import mePic from '../IMG/wave.png';
+import meExPic from '../IMG/Exp.png';
+import Carousel from 'react-bootstrap/Carousel'
 
 function HomePage(props) {
   return (
     <div>
-      <div className='keepHome'>
 
-        <h3 className='webText homeHeader'> Welcome to My Website! </h3>
-        <br />
+      <br />
+      <h3 className='webText homeHeader'> Welcome to My Website! </h3>
+      <br />
 
-        <img className='mePic' src={mePic} alt='me' />
+      <div className='caroContainer'>
 
-        <p className='homeText ind'> My name is Andaiye Wurm and I graduated Brooklyn College in Spring of 2022 with a Bachelors of Science in Multimedia Computing. I like to be creative and use my art skills to make cool and interesting websites. Click on the other pages to find out more about the websites I have made in the past!</p>
+        <Carousel>
 
-        <p className='homeText'> <span className='homeBig'> Interests and Hobbies </span></p>
+          <Carousel.Item>
+            <div className='caroSpace'>
+              <img className='mePic' src={mePic} alt='me' />
+              <p className='homeText'> <span className='homeBig'> Introduction </span></p>
+              <p className='homeText'> My name is Andaiye Wurm and I graduated Brooklyn College in Spring of 2022 with a Bachelors of Science in Multimedia Computing. I like to be creative and use my art skills to make cool and interesting websites. <br />Click on the other pages to find out more about the websites I have made in the past!</p>
+            </div>
+          </Carousel.Item>
 
-        <ul className='homeText'>
-          <li><u>Art:</u> I love to draw digitally. The drawings you see on this page are made by me as well.</li>
-          <li><u>Figure Skating:</u> I love figure skating. My favorite figure skater is Shoma Uno and is the reason I decided to learn Japanese!</li>
-          <li><u>Video Games:</u> I love to play games in my free time. My favorite game of all time is Kingdom Hearts II. </li>
-        </ul>
+          <Carousel.Item>
+            <div className='caroSpace'>
+              <img className='meExPic' src={meExPic} alt='me' />
+              <p className='homeText'> <span className='homeBig'> Interests and Hobbies </span></p>
+              <ul className='homeText'>
+                <li><u>Art:</u> I love to draw digitally. The drawings you see on this page are made by me as well.</li>
+                <li><u>Figure Skating:</u> I love figure skating. My favorite figure skater is Shoma Uno and is the reason I decided to learn Japanese!</li>
+                <li><u>Video Games:</u> I love to play games in my free time. My favorite game of all time is Kingdom Hearts II. </li>
+              </ul>
+            </div>
+          </Carousel.Item>
+
+        </Carousel>
 
       </div>
 
--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -3,52 +3,61 @@ import Accordion from 'react-bootstrap/Accordion';
 import Drama1 from '../IMG/Drama1.png';
 import Drama2 from '../IMG/Drama2.png';
 import Drama3 from '../IMG/Drama3.png';
+import HHub from '../IMG/HHub.png';
+import HHub2 from '../IMG/HHub2.png';
+import HHub3 from '../IMG/HHub3.png';
+import hsj from '../IMG/hsj.png';
+import hsj2 from '../IMG/hsj2.png';
+import hsj3 from '../IMG/hsj3.png';
+
 
 function ProjectsPage(props) {
   return (
     <div>
 
       <p className='webText projFirst'>All items are displayed in order from most recent to the oldest.</p>
-      <div className='accord'>
+      <div className='accord shadow'>
 
         <Accordion defaultActiveKey="0">
           <Accordion.Item eventKey="0">
             <Accordion.Header><span className='AccText'>Drama Tracker</span></Accordion.Header>
             <Accordion.Body>
-              <img src={Drama1} alt="Drama Tracker" className='projectIMG'/>
-              <img src={Drama2} alt="Drama Tracker" className='projectIMG'/>
-              <img src={Drama3} alt="Drama Tracker" className='projectIMG'/>
+              <span className='projSep'><img src={Drama1} alt="Drama Tracker" className='projectIMG shadow' /></span>
+              <span className='projSep'><img src={Drama2} alt="Drama Tracker" className='projectIMG shadow' /></span>
+              <span className='projSep'><img src={Drama3} alt="Drama Tracker" className='projectIMG shadow' /></span>
               <p className='webText accDes'>
                 Tech Used: React, Bootstrap, HTML, CSS, JavaScript, Sequelize, Postgres, Node.js and Express.
-                <br/><br/>
+                <br /><br />
                 Users are able to track the Japanese TV Shows and Movies that they are watching, completed, or plan to watch all in one place. The user is only allowed to use this feature if they are logged in. Otherwise they are just able to browse the available content.
-                <br/><br/>
-                <a href='https://drama-tracker-andy.herokuapp.com/' target="_blank"  className='linkText' >Link to Website</a>
+                <br /><br />
+                <a href='https://drama-tracker-andy.herokuapp.com/' target="_blank" className='linkText' >Link to Website</a>
               </p>
             </Accordion.Body>
           </Accordion.Item>
+
           <Accordion.Item eventKey="1">
             <Accordion.Header><span className='AccText'>Hobbies Hub</span></Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-              est laborum.
+              <span className='projSep'><img src={HHub} alt="Hobbies Hub" className='projectIMG shadow' /></span>
+              <span className='projSep'><img src={HHub2} alt="Hobbies Hub" className='projectIMG shadow' /></span>
+              <span className='projSep'><img src={HHub3} alt="Hobbies Hub" className='projectIMG shadow' /></span>
+              <p className='webText accDes'>
+                Tech Used: React, Bootstrap, HTML, CSS, JavaScript, Postgres, Node.js and Express.
+                <br /><br />
+                Users are able to meet other people with similar hobbies or interests. Once they sign up they will be instantly matched to someone else based on that interest and their age. They will then begin to chat with the other person to hopefully work together on their shared hobby. </p>
             </Accordion.Body>
           </Accordion.Item>
+
           <Accordion.Item eventKey="2">
             <Accordion.Header><span className='AccText'>Hey! Say! JUMP</span></Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-              est laborum.
+              <span className='projSep'><img src={hsj} alt="HeySayJump" className='projectIMG shadow' /></span>
+              <span className='projSep'><img src={hsj2} alt="HeySayJump" className='projectIMG shadow' /></span>
+              <span className='projSep'><img src={hsj3} alt="HeySayJump" className='projectIMG shadow' /></span>
+              <p className='webText accDes'>
+                Tech Used: HTML, CSS and JavaScript.
+                <br /><br />
+                Users are able to learn more about the Japanese band Hey! Say! JUMP. They can see which programs they are on for that month as well as look at pictures in the gallery. There is also a store section where the user can choose which piece of merch they want, add it to their cart, pay, and check out. </p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
